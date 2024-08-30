97e761180bfb4b82e371e612c4fb8d6ae02c387a
KP17893 <krishpathak23725@gmail.com>
2024-08-30 23:55:18 +0530

    navbar

--- frontend/src/Header1.jsx
+++ frontend/src/Header1.jsx
@@ -1,18 +1,16 @@
 import react from "react";
+import Logo from "./assets/logo.svg";
 
-
-function Header(){
+function Header1(){
     return(
         <div>
-            <header  className="p-3 text-bg-light" >
+            <header className="p-3 text-bg-light " styles={{borderBottom: "2px solid black"}}>
 
-                <div className="container" >
+                <div className="container">
                     <div
                         className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                         <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                            <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
-
-                            </svg>
+                            <img src={Logo} alt="logo" className="mr-2" alt="logo" />
                         </a>
 
                         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
@@ -24,17 +22,18 @@ function Header(){
                         </ul>
 
 
-
                         <div className="text-end">
                             <button type="button" className="btn btn-outline-dark me-2">Login</button>
                             <button type="button" className="btn btn-dark">Sign-up</button>
                         </div>
                     </div>
                 </div>
+                <hr />
             </header>
+
         </div>
     )
 }
 
-export default Header;
+export default Header1;
 
