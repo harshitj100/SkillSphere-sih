2c5e297d84f4d7449d8b5718e895925b5e4d6a03
harshit jain <harshitjainmahi@gmail.com>
2024-09-01 18:20:14 +0530

    adding course and v-lab

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,36 +2,44 @@ import React from 'react'
 import "./styles/styles.css";
 import Home from "./Home";
 import About from "./About";
-import {Link, Route, Router, Routes} from "react-router-dom";
-import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import V_lab from './V_lab';
+import Course from './Course'
+import { Link, Route, Router, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Coverpage from "./Coverpage";
 
 
 function App() {
-    const router = createBrowserRouter([
-        {
-            path: "/",
-            element:<Coverpage />,
-        },
-        {
-            path:"/home",
-            element:<Home />
-        },
-        {
-            path:"/about",
-            element:<About/>
-        }
-    ]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Coverpage />,
+    },
+    {
+      path: "/home",
+      element: <Home />
+    },
+    {
+      path: "/about",
+      element: <About />
+    }, {
+      path: "/virtual-lab",
+      element: <V_lab />
+    },{
+      path: "/course",
+      element: <Course />
+    }
+  ]);
 
-    return (
-        <RouterProvider router={router}>
-            <Routes>
-                <Route path="/" element={<Coverpage />} />
-                <Route path="/home" element={<Home />} />
+  return (
+    <RouterProvider router={router}>
+      <Routes>
+        <Route path="/" element={<Coverpage />} />
+        <Route path="/home" element={<Home />} />
 
-            </Routes>
-        </RouterProvider>
-    );
+      </Routes>
+    </RouterProvider>
+  );
 }
 
 export default App;
